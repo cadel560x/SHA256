8dfce7a1dbb4fea478789ef300005dba86085437
J M <g00329649@gmit.ie>
2019-02-19 22:09:47 +0000

    bitprint function initial commit

--- extras/bits.c
+++ extras/bits.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void bitprint(uint32_t x) {
+	int i;
+
+	for ( i = 0; i < 32; i++ ) {
+		if (x & (1 < (31 - i))) {
+			printf('1');
+		}
+		else {
+			printf('0');
+		}
+	}
+
+} // end function
+
+
 int main(int argc, char *argv[]) {
 
 	uint32_t a = 1;
 	uint32_t b = 2;
+
+	printf("a:  %08x\n", a);
+	printf("b:  %08x\n", b);
+
+	printf("&:  %08x\n",(a&b));
+	printf("|:  %08x\n",(a|b));
+	printf("^:  %08x\n",(a^b));
+	printf("~:  %08x\n",(~a));
+	printf("<<: %08x\n",(a<<1));
+	printf(">>: %08x\n",(a>>1));
 
 	return 0;
 
