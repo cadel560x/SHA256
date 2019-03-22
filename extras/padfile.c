e848370f771f946a9276caf63f3f09f8aea240f7
J M <g00329649@gmit.ie>
2019-03-22 14:50:28 +0000

    Padfile with bit and bytes counters

--- extras/padfile.c
+++ extras/padfile.c
@@ -11,6 +11,7 @@ union msgblock {
 int main(int argc, char *argv[]) {
 	union msgblock M;
 
+	uint64_t nobits = 0;
 	uint64_t nobytes;
 
 	FILE *f;
@@ -19,9 +20,25 @@ int main(int argc, char *argv[]) {
 
 	while (!feof(f)) {
 		nobytes = fread(M.e, 1, 64, f);
-		printf("%llu\n", nobytes);
+		printf("Read %2llu bytes\n", nobytes);
+		nobits = nobits + (nobytes * 8);
+		if (nobytes < 56 ) {
+			printf("I've found a block with less than 55 bytes!\n");
+			M.e[nobytes] = 0x80;
+			while (nobytes < 56) {
+				nobytes = nobytes + 1;
+				M.e[nobytes] = 0x00;
+			}
+			M.s[7] = nobits;
+		}
 
 	} // end loop
+
+	for (int i = 0; i < 64; i++) {
+		printf("%x ", M.e[i]);
+	}
+
+	printf("\n");
 
 	fclose(f);
 
